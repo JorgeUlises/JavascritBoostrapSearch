56c3d87c28a0823bfe236960ca1e4729239f30c1
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-04-26 12:53:25 -0500

    Inicial buscador

---
+++ script.js
@@ -0,0 +1,88 @@
+
+var enlacesRol = [{"etiqueta":"Coordinador - Estudiantes - Certificado de Estudio","url":"http://google.com"},{"etiqueta":"Coordinador - Estudiantes - Certificados Internos","url":"http://google.com"},{"etiqueta":"Coordinador - Estudiantes - Codificar Estudiantes Nuevos","url":"http://google.com"},{"etiqueta":"Coordinador - Estudiantes - Datos Básicos","url":"http://google.com"},{"etiqueta":"Coordinador - Estudiantes - Historia Académica","url":"http://google.com"},{"etiqueta":"Coordinador - Estudiantes - Horario Estudiantes","url":"http://google.com"},{"etiqueta":"Coordinador - Estudiantes - Inscripción a grado","url":"http://google.com"},{"etiqueta":"Coordinador - Estudiantes - Recalcular Reglamento","url":"http://google.com"},{"etiqueta":"Coordinador - Docentes - Consulta observaciones","url":"http://google.com"},{"etiqueta":"Coordinador - Docentes - Consulta observaciones 2014-1 +","url":"http://google.com"},{"etiqueta":"Coordinador - Docentes - Enviar Correo","url":"http://google.com"},{"etiqueta":"Coordinador - Docentes - Evaluar Docentes","url":"http://google.com"},{"etiqueta":"Coordinador - Docentes - Listado de Docentes","url":"http://google.com"},{"etiqueta":"Coordinador - Docentes - Observaciones Estudiantes","url":"http://google.com"},{"etiqueta":"Coordinador - Docentes - Plan de Trabajo","url":"http://google.com"},{"etiqueta":"Coordinador - Consejerías - Administración","url":"http://google.com"},{"etiqueta":"Coordinador - Consejerías - Mensajes","url":"http://google.com"},{"etiqueta":"Coordinador - Horarios - Disponibilidad de cupos","url":"http://google.com"},{"etiqueta":"Coordinador - Horarios - Gestionar Horarios","url":"http://google.com"},{"etiqueta":"Coordinador - Horarios - Horarios Por Grupo","url":"http://google.com"},{"etiqueta":"Coordinador - Horarios - Ocupación de Salones","url":"http://google.com"},{"etiqueta":"Coordinador - Carga Académica - Gestionar Carga","url":"http://google.com"},{"etiqueta":"Coordinador - Notas - Fechas Notas Parciales","url":"http://google.com"},{"etiqueta":"Coordinador - Notas - Histórico de Notas","url":"http://google.com"},{"etiqueta":"Coordinador - Notas - Notas digitadas","url":"http://google.com"},{"etiqueta":"Coordinador - Notas - Novedades de Notas","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Adiciones y Cancelaciones","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Cursos intermedios","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Cursos Intermedios","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Estudiantes Inactivos","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Inscripcion nuevos","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Parámetros Condor","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Preinscripción Automática","url":"http://google.com"},{"etiqueta":"Coordinador - Inscripciones - Preins. por Demanda","url":"http://google.com"},{"etiqueta":"Coordinador - Cierre de Semestre - Cargar Notas","url":"http://google.com"},{"etiqueta":"Coordinador - Cierre de Semestre - Cerrar Semestre","url":"http://google.com"},{"etiqueta":"Coordinador - Plan de Estudios - Config Planes de Estudios créditos","url":"http://google.com"},{"etiqueta":"Coordinador - Plan de Estudios - Consultar planes de estudios créditos","url":"http://google.com"},{"etiqueta":"Coordinador - Plan de Estudios - Consultar planes de Estudios Horas","url":"http://google.com"},{"etiqueta":"Coordinador - Plan de Estudios - Homologaciones","url":"http://google.com"},{"etiqueta":"Coordinador - Datos Proyecto Curricular - Datos Proyecto curricular","url":"http://google.com"},{"etiqueta":"Coordinador - Recibos de Pago - Histórico Recibos de Pago matrículas","url":"http://google.com"},{"etiqueta":"Coordinador - Recibos de Pago - Recibos de Pago","url":"http://google.com"},{"etiqueta":"Coordinador - Recibos de Pago - Recibos otros derechos","url":"http://google.com"},{"etiqueta":"Coordinador - Recibos de Pago - SaberPro",
+  "url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Docentes nuevos","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Encriptar Aspirantes","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Encriptar claves","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Estudiantes nuevos","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Funcionarios nuevos","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Log Adiciones y Cancelaciones","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Perfiles","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Revisión","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Usuario Activos","url":"http://google.com"},{"etiqueta":"Administrador OAS - Administración de Usuarios - Usuario Nuevo","url":"http://google.com"},{"etiqueta":"Administrador OAS - Intensidad Horaria - Actualización Intensidad Egresado","url":"http://google.com"},{"etiqueta":"Administrador OAS - Intensidad Horaria - Actualización Intensidad Estudiante","url":"http://google.com"},{"etiqueta":"Administrador OAS - Intensidad Horaria - Intensidad Lote","url":"http://google.com"},{"etiqueta":"Administrador OAS - Intensidad Horaria - Preinscripción por Demanda","url":"http://google.com"},{"etiqueta":"Administrador OAS - Docentes - Coordinadores","url":"http://google.com"},{"etiqueta":"Administrador OAS - Docentes - Corregir cédulas","url":"http://google.com"},{"etiqueta":"Administrador OAS - Docentes - Decanos","url":"http://google.com"},{"etiqueta":"Administrador OAS - Docentes - Docentes","url":"http://google.com"},{"etiqueta":"Administrador OAS - Modelo del Riesgo - Ejecutar Proceso Modelo de Riesgo","url":"http://google.com"},{"etiqueta":"Administrador OAS - Servicios - Cierres Semestres","url":"http://google.com"},{"etiqueta":"Administrador OAS - Servicios - Encriptar caracteres","url":"http://google.com"},{"etiqueta":"Administrador OAS - Servicios - Enviar Recibos","url":"http://google.com"},{"etiqueta":"Administrador OAS - Servicios - Estudiantes activos","url":"http://google.com"},{"etiqueta":"Administrador OAS - Servicios - Estudiantes inactivos con asignaturas inscritas","url":"http://google.com"},{"etiqueta":"Administrador OAS - Servicios - Fechas de Eventos","url":"http://google.com"},{"etiqueta":"Soporte OAS - Estudiantes - Actualizar datos est. 004","url":"http://google.com"},{"etiqueta":"Soporte OAS - Estudiantes - Certificados Internos","url":"http://google.com"},{"etiqueta":"Soporte OAS - Estudiantes - Consultar Preins","url":"http://google.com"},{"etiqueta":"Soporte OAS - Estudiantes - Historia Académica","url":"http://google.com"},{"etiqueta":"Soporte OAS - Estudiantes - Histórico Recibos de Pago","url":"http://google.com"},{"etiqueta":"Soporte OAS - Estudiantes - Recalcular Reglamento","url":"http://google.com"},{"etiqueta":"Soporte OAS - Estudiantes - Validar Estudiantes Activos","url":"http://google.com"},{"etiqueta":"Soporte OAS - Administración de Usuarios - Bitácora","url":"http://google.com"},{"etiqueta":"Soporte OAS - Administración de Usuarios - Habilitar Recuperar Clave","url":"http://google.com"},{"etiqueta":"Soporte OAS - Administración de Usuarios - Reporte Atencion Usuarios","url":"http://google.com"},{"etiqueta":"Soporte OAS - Administración de Usuarios - Solicitud Usuarios","url":"http://google.com"},{"etiqueta":"Soporte OAS - Intensidad Horaria - Actualización Intensidad","url":"http://google.com"},{"etiqueta":"Soporte OAS - Homologaciones - Hom. pendientes","url":"http://google.com"},{"etiqueta":"Soporte OAS - Grados - Cargar archivo","url":"http://google.com"},{"etiqueta":"Soporte OAS - Docentes - Documentos","url":"http://google.com"},{"etiqueta":"Soporte OAS - Inscripciones - Actualizar Preins.","url":"http://google.com"},
+  {"etiqueta":"Soporte OAS - Inscripciones - Inscripción Automática","url":"http://google.com"},{"etiqueta":"Soporte OAS - Inscripciones - Procesar y cargar datos","url":"http://google.com"},{"etiqueta":"Soporte OAS - Recibos de pago - Cambiar estado","url":"http://google.com"},{"etiqueta":"Soporte OAS - Recibos de pago - Generar Recibo de Pago","url":"http://google.com"},{"etiqueta":"Soporte OAS - Recibos de pago - Recibos de Pago","url":"http://google.com"},{"etiqueta":"Soporte OAS - Recibos de pago - Recibos especiales","url":"http://google.com"},{"etiqueta":"Soporte OAS - Horarios - Admin Horarios","url":"http://google.com"},{"etiqueta":"Soporte OAS - Horarios - Ocupación de salones","url":"http://google.com"},{"etiqueta":"Soporte OAS - Modelo del Riesgo - Ejecutar Proceso","url":"http://google.com"},{"etiqueta":"Soporte OAS - Certificados - Ingresos y Retencion","url":"http://google.com"},{"etiqueta":"Asistente Contabilidad - Recibos de Pago - Histórico Recibos de Pago","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Activos","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Adición y Cancelación","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Asignaturas Inscritas","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Certificados de Estudio","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Certificados Internos","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Codif. Estud. Nuevos","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Con Asig. Inscritas","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Datos Básicos","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Historia Académica","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Histórico Recibos de Pago","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Inscripción a grado","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - Recalcular Reglamento","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estudiantes - SaberPro","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Docentes - Listado de Docentes","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Horarios - Gestionar Horarios","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Horarios - Horarios Por Grupo","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Horarios - Ocupación de salones","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Recibos de Pago - Recibos de Pago","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Noticias - Noticias","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estadísticas - Deserción","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estadísticas - Funcionarios","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Estadísticas - Proceso Admisiones","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Plan de Estudios - Homologaciones","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Carga Académica - Gestionar Carga","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Inscripciones - Cursos Intermedios","url":"http://google.com"},{"etiqueta":"Asistente de Proyecto - Inscripciones - Horario Estudiantes","url":"http://google.com"},{"etiqueta":"Asistente Secretaría Facultad - Estudiantes - Certificados Internos","url":"http://google.com"},{"etiqueta":"Asistente Secretaría Facultad - Estudiantes - Historia Académica","url":"http://google.com"},{"etiqueta":"Asistente Secretaría Facultad - Estudiantes - Sabana de notas","url":"http://google.com"},{"etiqueta":"Asistente Secretaría Facultad - Egresados - Generar Acta de grado","url":"http://google.com"},{"etiqueta":"Asistente Secretaría Facultad - Egresados - Generar Copia de Acta de grado","url":"http://google.com"},
+  {"etiqueta":"Secretaria de Proyecto - Estudiantes - Certificado de Estudio","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Estudiantes - Certificados Internos","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Estudiantes - Codif. Estud. Nuevos","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Estudiantes - Historia Académica","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Estudiantes - Histórico Recibos de Pago","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Estudiantes - Inscripción a grado","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Horarios - Horarios Por Grupo","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Horarios - Ocupación de salones","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Plan de Estudios - Homologaciones","url":"http://google.com"},{"etiqueta":"Secretaria de Proyecto - Inscripciones - Horario Estudiantes","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Bienestar","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Docencia","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Facultad","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - ILUD","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Laboratorios","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Posgrados","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Proyecto curricular","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Rel. Interinstitucionales","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Secretaria academica","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Soporte","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Tesoreria","url":"http://google.com"},{"etiqueta":"Reportes - Reportes - Vicerrectoria academica","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación 2016 - Calendario Académico","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación 2016 - Tabla de Valores de Derechos Pecuniarios","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación Docente - Acuerdo 04 de 2011","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación Docente - Estatuto Estudiantil","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación Docente - Trabajos de Grado","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación Estudiantes - Descarga horas lectivas docentes","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación Estudiantes - Descarga horas lectivas docentes carrera","url":"http://google.com"},{"etiqueta":"Documentos - Reglamentación Estudiantes - Estatuto Docente","url":"http://google.com"}];
+
+function showResult(evt){
+  console.log(evt);
+  var criterio = evt.target.value;
+  if (criterio.length<4) {
+	  cleanResult();
+	  return;
+  }
+  setResult(criterio);
+}
+
+function cleanResult(){
+  $("#livesearch").css("border","0px");
+  $("#livesearch").css("display","none");
+}
+
+function setResult(criterio){
+  console.log(criterio);
+  searchResults(criterio);
+  $("#livesearch").css("border","1px solid #A5ACB2");
+  $("#livesearch").css("display","block");
+}
+
+function addItemResult(enlace){
+  var link = "<a target='principal' href='"+enlace.url+"'>"+enlace.etiqueta+"</a><br />";
+  $("#livesearch").append(link);
+}
+
+function searchResults(criterio){
+  $("#livesearch").html("");
+  var numEnlaces = 0;
+  //enlacesRol es una variable que viene del javascript del bloque menuPrincipal
+  for(var i in enlacesRol){
+	  var indice = enlacesRol[i].etiqueta.toUpperCase().indexOf(criterio.toUpperCase());
+	  if(indice > -1){
+		  addItemResult(enlacesRol[i]);
+		  numEnlaces++;
+		  if(numEnlaces >= numeroResultados){
+			  markSelected();
+			  return;
+		  }
+	  }
+  }
+  markSelected();
+}
+
+function markSelected(){
+  console.log(keyPosition);
+  var selectedLink = $("#livesearch a")[keyPosition];
+  while(selectedLink==undefined){
+	  if(keyPosition>0){
+		  keyPosition = 0;
+	  } else {
+		  keyPosition = keyPosition - 2;
+	  }
+	  selectedLink = $("#livesearch a")[keyPosition];
+  }
+  console.log(selectedLink);
+  $(selectedLink).attr("data-value","selected");
+  $(selectedLink).css("background-color","#f00");
+}
+
+var keyPosition = 0;
+var numeroResultados = 5;
+function spetialNavigation(evt){
+  var key = evt.key;
+  if (key=="ArrowUp"){
+    keyPosition--;
+    if(keyPosition < 0){
+      keyPosition = numeroResultados - 1;
+    }
+  } else if (key=="ArrowDown"){
+    keyPosition++;
+    if(keyPosition >= numeroResultados){
+      keyPosition = 0;
+    }
+  }	
+}
+
+$("#searchservice").keyup(showResult);
+$("#searchservice").keypress(spetialNavigation);
+$("#searchservice").parent().mouseleave(cleanResult);
+
